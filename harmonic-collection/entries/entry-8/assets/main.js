6f9bbd30c4c7a0b0bdd97535a4ae1b8e4e800108
eemilyyao <em.yao@hotmail.com>
2022-11-17 19:32:36 -0500

    1117

---
+++ harmonic-collection/entries/entry-8/assets/main.js
@@ -0,0 +1,8 @@
+
+let agnesplay = function(){document.getElementById("agnesaudio").play()}
+
+let toobigplay = function(){document.getElementById("toobigaudio").play()}
+
+let scissorplay = function(){document.getElementById("scissoraudio").play()}
+
+let perfectplay = function(){document.getElementById("perfectaudio").play()}
